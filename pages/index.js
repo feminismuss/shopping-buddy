2202230eb1983b57066649fbd7b190f7a823dbf5
Hannah Capell <hannah.capell@gmail.com>
2025-07-23 16:38:08 +0200

    global fetcher in app.js

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,21 @@
+import styled from "styled-components";
+import useSWR from "swr";
+import { StyledLink } from "@/components/StyledLink";
+
+const ListContainer = styled.ul`
+  list-style: none;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 20px;
+  width: 100%;
+`;
+const FixedLink = styled(StyledLink)`
+  position: fixed;
+  bottom: 50px;
+  right: 50px;
+`;
+
+
 export default function HomePage() {
   return (
     <div>
