7c9409fe590994f5e10dbffc19e6e5d508b972bf
Hannah Capell <hannah.capell@gmail.com>
2025-07-28 17:02:34 +0200

    add page for addItem

---
+++ pages/addItem.js
@@ -0,0 +1,25 @@
+import { useRouter } from "next/router";
+import ItemForm from "../components/ItemForm";
+
+export default function AddShoppingItem() {
+  const router = useRouter();
+
+  async function handleAddShoppingItem(data) {
+    const response = await fetch("/api/shoppingItems", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+    if (response.ok) {
+      router.push("/");
+    } else {
+      console.error("Save failed");
+    }
+  }
+  return (
+    <>
+      <h1>Add new Item</h1>
+      <ItemForm onSubmit={handleAddShoppingItem} formName="new-item-form" />
+    </>
+  );
+}
